c67bd12abfcf731425ad3b1d2e9ba128669c2772
최정헌 <iiaii@zuminternet.com>
2020-07-16 09:25:06 +0900

    vuex setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ count }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state:{
+    count: 12,
+  },
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  store,
+}).$mount('#app');
+
